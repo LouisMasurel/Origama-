badf9da25502d336ce51dfff93b6062ec4768031
Louis Masurel <masurellouis@gmail.com>
2018-04-06 16:07:06 +0200

    done

--- src/canvasorigama.js
+++ src/canvasorigama.js
@@ -1,4 +1,9 @@
 // Initial Setup
+
+
+
+
+  
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
@@ -11,19 +16,12 @@ addEventListener('resize', () => {
     init()
 })
 
-addEventListener('foodAssimilation', () => {
-  hasAssimilated = true
-    setTimeout(function(){
-        userPressedKey = false
-    }, 15)
-})
-
 // // ----- OBJECTS ----- // //
 
 //------ function mouvement ------ //
 
 // Objects
-function Particule (radius, distance, velocity, radian, color, name) {
+function Particule (radius, distance, velocity, radian, color, name, song) {
     x = canvas.width/2 ;
     y = canvas.height/2;
     this.x = x;    
@@ -34,6 +32,7 @@ function Particule (radius, distance, velocity, radian, color, name) {
     this.velocity = velocity;
     this.radians = radian;
     this.name = name;
+    this.song = song;
 
     Object.prototype.update = function() {
         this.radians += this.velocity; 
@@ -54,87 +53,86 @@ function Particule (radius, distance, velocity, radian, color, name) {
 // Implementation
 
 var R = (2.0*Math.PI)/360;
-var t = innerHeight/2-70;
-var s = innerHeight / 150;
-var v = 0.0002;
+var t = innerHeight/2-innerHeight/10;
+var s = innerHeight / 110;
+var v = 0.0025;
 
 var allParticules ;
 function init() {
   allParticules = [
                          // radius, distance, velocity, radian:x*(2.0*Math.PI)/360, color, class, name
-        new Particule (p=(s*2), m=(t*1/10), w = -(v+0.0006), 0, purple = 'grey', 'purple'),
-        new Particule (p, m, w, (360/3)*R, purple, 'purple'),
-        new Particule (p, m, w, (360/3*2)*R, purple, 'purple'),
+        new Particule (p=(s*2), m=(t*0.8/10), w = (v+0.009), 0, green = 'grey', 'green', 'none'),
+        new Particule (p, m, w, (360/3)*R, green, 'green', 'none'),
+        new Particule (p, m, w, (360/3*2)*R, green, 'green', 'none'),
         
-        new Particule (r=s*8, d=t*3/9, v=(v+0.0004), 0, green = 'grey', 'green'),
-        new Particule (r, d, v, (360/5)*R, green, 'green'),
-        new Particule (r, d, v, (360/5*2)*R, green, 'green'),
-        new Particule (r, d, v, (360/5*3)*R, green, 'green'),
-        new Particule (r, d, v, (360/5*4)*R, green, 'green'),
+        new Particule (g=s*3.5, d=t*1.5/9, j=(v+0.007), 0, purple= 'grey', 'purple', 'none'),
+        new Particule (g, d, j, (360/5)*R, purple, 'purple', 'none'),
+        new Particule (g, d, j, (360/5*2)*R, purple, 'purple', 'none'),
+        new Particule (g, d, j, (360/5*3)*R, purple, 'purple', 'none'),
+        new Particule (g, d, j, (360/5*4)*R, purple, 'purple', 'none'),
 
-        new Particule (g=s*2, d=t*3/9, j=-(v+0.0001), 0, grey= 'grey', 'grey'),
-        new Particule (g, d, j, (360/5)*R, grey, 'grey'),
-        new Particule (g, d, j, (360/5*2)*R, grey, 'grey'),
-        new Particule (g, d, j, (360/5*3)*R, grey, 'grey'),
-        new Particule (g, d, j, (360/5*4)*R, grey, 'grey'),
+        new Particule (r=s*4.5, d=t*2.5/9, b=(v+0.004), 0, darkBlue = 'grey', 'darkBlue', 'none'),
+        new Particule (r, d, b, (360/5)*R, darkBlue, 'darkBlue', 'none'),
+        new Particule (r, d, b, (360/5*2)*R, darkBlue, 'darkBlue', 'none'),
+        new Particule (r, d, b, (360/5*3)*R, darkBlue, 'darkBlue', 'none'),
+        new Particule (r, d, b, (360/5*4)*R, darkBlue, 'darkBlue', 'none'),
+
+        new Particule (a=(s*5.5), i=t*4/9, e=(v+0.002), 0, turquoise = 'grey', 'turquoise', 'none'),
+        new Particule (a, i, e, (360/15*3)*R, turquoise, 'turquoise', 'none'),
+        new Particule (a, i, e, (360/15*6)*R, turquoise, 'turquoise', 'none'),
+        new Particule (a, i, e, (360/15*9)*R, turquoise, 'turquoise', 'none'),
+        new Particule (a, i, e, (360/15*12)*R, turquoise, 'turquoise', 'none'),
+
+        new Particule (ai=s*3, ii=t*6/9-(a/2), é=(e+0.001), (360/15)*R, blue = 'grey', 'blue', 'none'),
+        new Particule (ai, ii, é, (360/15*2)*R, blue, 'blue', 'none'),
+        new Particule (ai, ii, é, (360/15*4)*R, blue, 'blue', 'none'),
+        new Particule (ai, ii, é, (360/15*5)*R, blue, 'blue', 'none'),
+        new Particule (ai, ii, é, (360/15*7)*R, blue, 'blue', 'none'),
+        new Particule (ai, ii, é, (360/15*8)*R, blue, 'blue', 'none'),
+        new Particule (ai, ii, é, (360/15*10)*R, blue, 'blue', 'none'),
+        new Particule (ai, ii, é, (360/15*11)*R, blue, 'blue', 'none'),
+        new Particule (ai, ii, é, (360/15*13)*R, blue, 'blue', 'none'),
+        new Particule (ai, ii, é, (360/15*14)*R, blue, 'blue', 'none'),
 
 
-        new Particule (a=(s*4.5), i=t*6/9, e=(v+0.0002), 0, turquoise = 'grey', 'turquoise'),
-        new Particule (a, i, e, (360/15*3)*R, turquoise, 'turquoise'),
-        new Particule (a, i, e, (360/15*6)*R, turquoise, 'turquoise'),
-        new Particule (a, i, e, (360/15*9)*R, turquoise, 'turquoise'),
-        new Particule (a, i, e, (360/15*12)*R, turquoise, 'turquoise'),
+        new Particule (u=s*4, j=t*7/9, l=(é+0.0005), 10*R, red = 'grey', 'red', 'none'),
+        new Particule (u, j, l, 80*R, red, 'red', 'none'),
+        new Particule (u, j, l, 100*R, red, 'red', 'none'),
+        new Particule (u, j, l, 170*R, red, 'red', 'none'),
+        new Particule (u, j, l, 190*R, red, 'red', 'none'),
+        new Particule (u, j, l, 260*R, red, 'red', 'none'),
+        new Particule (u, j, l, 280*R, red, 'red', 'none'),
+        new Particule (u, j, l, 350*R, red, 'red', 'none'),
 
-        new Particule (ai=s*3, ii=t*6/9-(a/2), -e, (360/15)*R, blue = 'grey', 'blue'),
-        new Particule (ai, ii, -e, (360/15*2)*R, blue, 'blue'),
-        new Particule (ai, ii, -e, (360/15*4)*R, blue, 'blue'),
-        new Particule (ai, ii, -e, (360/15*5)*R, blue, 'blue'),
-        new Particule (ai, ii, -e, (360/15*7)*R, blue, 'blue'),
-        new Particule (ai, ii, -e, (360/15*8)*R, blue, 'blue'),
-        new Particule (ai, ii, -e, (360/15*10)*R, blue, 'blue'),
-        new Particule (ai, ii, -e, (360/15*11)*R, blue, 'blue'),
-        new Particule (ai, ii, -e, (360/15*13)*R, blue, 'blue'),
-        new Particule (ai, ii, -e, (360/15*14)*R, blue, 'blue'),
+        new Particule (ù=(u*1.5), j, l, 45*R, redSeconde= 'grey', 'orange', 'none'),
+        new Particule (ù, j, l, 135*R, redSeconde, 'rorange', 'none'),
+        new Particule (ù, j, l, 225*R, redSeconde, 'orange', 'none'), 
+        new Particule (ù, j, l, 315*R, redSeconde, 'orange', 'none'),
 
-
-        new Particule (u=s*2, j=t*8/9, l=(v+0.0001), 10*R, red = 'grey', 'red'),
-        new Particule (u, j, l, 80*R, red, 'red'),
-        new Particule (u, j, l, 100*R, red, 'red'),
-        new Particule (u, j, l, 160*R, red, 'red'),
-        new Particule (u, j, l, 190*R, red, 'red'),
-        new Particule (u, j, l, 260*R, red, 'red'),
-        new Particule (u, j, l, 280*R, red, 'red'),
-        new Particule (u, j, l, 350*R, red, 'red'),
-
-        new Particule (u*1.5, j, l, 45*R, redSeconde= 'grey', 'redSeconde'),
-        new Particule (u*1.5, j, l, 135*R, redSeconde, 'redSeconde'),
-        new Particule (u*1.5, j, l, 225*R, redSeconde, 'redSeconde'), 
-        new Particule (u*1.5, j, l, 315*R, redSeconde, 'redSeconde'),
-
-        new Particule (s, t, -v, 5*R, yellow = 'grey', 'yellow'),
-        new Particule (s, t, -v, 25*R, yellow, 'yellow'),
-        new Particule (s, t, -v, 35*R, yellow, 'yellow'),
-        new Particule (s, t, -v, 55*R, yellow, 'yellow'),
-        new Particule (s, t, -v, 65*R, yellow, 'yellow'),
-        new Particule (s, t, -v, 85*R, yellow, 'yellow'),
-        new Particule (s, t, -v, 95*R, yellow, 'yellow'),
-        new Particule (s, t, -v, 115*R, yellow, 'yellow'),
-        new Particule (s, t, -v, 125*R, yellow, 'yellow'),
-        new Particule (s, t, -v, 145*R, yellow, 'yellow'),
-        new Particule (s, t, -v, 155*R, yellow, 'yellow'),
-        new Particule (s, t, -v, 175*R, yellow, 'yellow'),
-        new Particule (s, t, -v, 185*R, yellow, 'yellow'),
-        new Particule (s, t, -v, 205*R, yellow, 'yellow'),
-        new Particule (s, t, -v, 215*R, yellow, 'yellow'),
-        new Particule (s, t, -v, 235*R, yellow, 'yellow'),
-        new Particule (s, t, -v, 245*R, yellow, 'yellow'),
-        new Particule (s, t, -v, 265*R, yellow, 'yellow'),
-        new Particule (s, t, -v, 275*R, yellow, 'yellow'),
-        new Particule (s, t, -v, 295*R, yellow, 'yellow'),
-        new Particule (s, t, -v, 305*R, yellow, 'yellow'),
-        new Particule (s, t, -v, 325*R, yellow, 'yellow'),
-        new Particule (s, t, -v, 335*R, yellow, 'yellow'),
-        new Particule (s, t, -v, 355*R, yellow, 'yellow'),
+        new Particule (h=(s*2), t, z=(l+0.0005), 5*R, yellow = 'grey', 'yellow', 'none'),
+        new Particule (k=(s*4), t, z, 25*R, yellow, 'yellow', 'none'),
+        new Particule (k, t, z, 35*R, yellow, 'yellow', 'none'),
+        new Particule (h, t, z, 55*R, yellow, 'yellow', 'none'),
+        new Particule (h, t, z, 65*R, yellow, 'yellow', 'none'),
+        new Particule (k, t, z, 85*R, yellow, 'yellow', 'none'),
+        new Particule (k, t, z, 95*R, yellow, 'yellow', 'none'),
+        new Particule (h, t, z, 115*R, yellow, 'yellow', 'none'),
+        new Particule (h, t, z, 125*R, yellow, 'yellow', 'none'),
+        new Particule (k, t, z, 145*R, yellow, 'yellow', 'none'),
+        new Particule (k, t, z, 155*R, yellow, 'yellow', 'none'),
+        new Particule (h, t, z, 175*R, yellow, 'yellow', 'none'),
+        new Particule (h, t, z, 185*R, yellow, 'yellow', 'none'),
+        new Particule (k, t, z, 205*R, yellow, 'yellow', 'none'),
+        new Particule (k, t, z, 215*R, yellow, 'yellow', 'none'),
+        new Particule (h, t, z, 235*R, yellow, 'yellow', 'none'),
+        new Particule (h, t, z, 245*R, yellow, 'yellow', 'none'),
+        new Particule (k, t, z, 265*R, yellow, 'yellow', 'none'),
+        new Particule (k, t, z, 275*R, yellow, 'yellow', 'none'),
+        new Particule (h, t, z, 295*R, yellow, 'yellow', 'none'),
+        new Particule (h, t, z, 305*R, yellow, 'yellow', 'none'),
+        new Particule (k, t, z, 325*R, yellow, 'yellow', 'none'),
+        new Particule (k, t, z, 335*R, yellow, 'yellow', 'none'),
+        new Particule (h, t, z, 355*R, yellow, 'yellow', 'none'),
     ]
     }
 
@@ -150,6 +148,7 @@ function animate() {
 
     init()
     animate()
+
 // // -----PIPES -----// //
 
 // ----- creation pipe ----- // 
@@ -175,32 +174,55 @@ function Pipe (myX, myY, myW, myH, color) {
 
 
 var allPipes = [
-  new Pipe (((innerWidth/2)-2.5), 65, 5, (innerHeight/2-65), 'brown'),
-  new Pipe ((innerWidth/2-15),(innerHeight-30), 30, 10, 'brown')
+  new Pipe (((innerWidth/2)-2.5), innerHeight/17, 5, ((innerHeight/2)-(innerHeight/15)), 'brown', 'none'),
+  new Pipe ((innerWidth/2-15),(innerHeight-innerHeight/20), innerWidth/17, innerHeight/80, 'brown')
 ];
 
 // ----- collision pipe and particule ----- //
 
   function getLeft (obj) {
-    return obj.x;
+    if(obj.width) {
+      return obj.x;
+    } else {
+      return obj.x - obj.radius;
+    }
   }
+
   function getRight (obj) {
-      if(obj.width) {
-          return obj.x + obj.width
-      } else {
-        return obj.x + obj.radius;
-      }
+    if(obj.width) {
+      return obj.x + obj.width;
+    } else {
+      return obj.x + obj.radius;
+    }
   }
+  
+  function getBottom (obj) {
+    if(obj.height) {
+      return obj.y + obj.height;
+    } else {
+      return obj.y + obj.radius;
+    }
+  };
+
+  function getTop (obj) {
+    if(obj.height) {
+      return obj.y;
+    } else {
+      return obj.y - obj.radius;
+    }
+  };
   
   function collision (objA, objB) {
-    return getRight(objA)  >= getLeft(objB)   &&
-           getLeft(objA)   <= getRight(objB);
+    return getBottom(objA) >= getTop(objB)    &&
+    getTop(objA)  <= getBottom(objB) &&
+    getRight(objA)  >= getLeft(objB)   &&
+    getLeft(objA)   <= getRight(objB);
   }
   
-  function pipeCollision (Particule) {
+  function pipeCollision (particule) {
     var hasCollided = false;
   
-    if (collision(Particule, allPipes[0])) {
+    if (collision(particule, allPipes[0])) {
         hasCollided = true;
       }
     return hasCollided;
@@ -210,16 +232,18 @@ var allPipes = [
 
 //----- creat FOOD  ----- //
 var food = 0 
-function Food (radius, color) {
-    this.x = null;
-    this.y = null;
+function Food (radius, color, song) {
+    this.x = 1;
+    this.y = 1;
     this.speed = 1;
     this.radius = radius;
     this.color = color;
+    this.song = song;
+    this.id = Food;
     this.creation();
     this.clearFood();
 }
-    
+
   Food.prototype.drawMe = function () {
     c.beginPath()
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
@@ -228,34 +252,47 @@ function Food (radius, color) {
     c.fill();
     c.closePath();
     };
-    
+  
   Food.prototype.clearFood = function () {
      c.beginPath();
-     c.clearRect(this.x, innerHeight-((innerHeight/7)/2), this.radius, 0, Math.PI * 2, false);
+     c.clearRect(this.x, this.y, this.radius, 0, Math.PI * 2, false);
      c.closePath();
     };
   
   Food.prototype.creation = function () {
-    this.y = 30; 
-    this.x = Math.floor(Math.random() * (canvas.width-(innerHeight/7)))
-       if ((innerHeight/7) > this.x) {
-        this.creation();
-        return;
-        };   
+    this.y = innerHeight/30; 
+    this.x = innerWidth/8 + Math.floor(Math.random() * (canvas.width-((2*innerWidth)/8)))   
       };
-
+  
+  // setTimeout(Food.prototype.creation(), 1000) 
+      
 var N = innerHeight / 100 
+var greenSound = document.querySelector(".greenSound");
+var purpleSound = document.querySelector(".purpleSound");
+var darkBlueSound = document.querySelector(".darkBlueSound");
+var blueSound = document.querySelector(".blueSound");
+var turquoiseSound = document.querySelector(".turquoiseSound");
+var redSound = document.querySelector(".redSound");
+var orangeSound = document.querySelector(".orangeSound");
+var yellowSound = document.querySelector(".yellowSound");
+
 
 var allFoods = [
-    new Food (N, 'purple'),
-    new Food (N, 'green'),
-    new Food (N, 'grey'),
-    new Food (N, 'blue'),
-    new Food (N, 'turquoise'),
-    new Food (N, 'red'),
-    new Food (N, 'redSeconde'),
-    new Food (N, 'yellow'),
+    new Food (N, 'green', greenSound),
+    new Food (N, 'purple', purpleSound ),
+    new Food (N, 'darkBlue', darkBlueSound),
+    new Food (N, 'blue', blueSound),
+    new Food (N, 'turquoise', turquoiseSound),
+    new Food (N, 'red', redSound),
+    new Food (N, 'orange', orangeSound),
+    new Food (N, 'yellow', yellowSound),
 ];
+var movingFoods = [];
+randomFoods();
+
+setInterval(function () {
+  randomFoods();
+}, 500);
 
 
 //----- mouvement and limite of Pipe and particules by button Arrow ----- //
@@ -278,126 +315,119 @@ var allFoods = [
         }
       };
 
-   
-  // ----- assimilation Pipe and food ----- //
+    // ----- assimilation Pipe and food ----- //
 
 
 function getTopSecond (obj) {
-  if(obj.width) {
-    return obj.x + obj.width
+  if(obj.height) {
+    return obj.y;
   } else {
-    return obj.x + obj.radius;
+    return obj.y - obj.radius;
   }
 };
 function getBottomSecond (obj) {
+  if(obj.height) {
+    return obj.y + obj.height;
+  } else {
+    return obj.y + obj.radius;
+  }
+};
+
+function getLeftSecond (obj) {
   if(obj.width) {
-    return obj.x + obj.width
+    return obj.x;
+  } else {
+    return obj.x - obj.radius;
+  }
+}
+function getRightSecond (obj) {
+  if(obj.width) {
+    return obj.x + obj.width;
   } else {
     return obj.x + obj.radius;
   }
-};
+}
 
 function assimilation (objA, objB) {
   return getBottomSecond(objA) >= getTopSecond(objB)    &&
-         getTopSecond(objA)    <= getTopSecond(objB) 
+          getTopSecond(objA)  <= getBottomSecond(objB) &&
+          getRightSecond(objA)  >= getLeftSecond(objB)   &&
+          getLeftSecond(objA)   <= getRightSecond(objB);
 };
   
-  function foodAssimilation (Food) {
+  function foodAssimilation (food) {
     var hasAssimilated = false;
-    console.log(Food)
     
-    if (assimilation(Food, allPipes[1])===true) {
+    if (assimilation(food, allPipes[1])) {
       hasAssimilated = true;
-        Food.clearRect();
-        return hasAssimilated;
+        // food.clearFood();
       }
-    return Food.color;
+    return hasAssimilated;
   }
-  
-  console.log(foodAssimilation(allFoods[2]))    
-  // ----- update action ----- // 
-  // allFoods.forEach(function foodAssimilation (Food){
-  //   var hasAssimilated = false;
-  //   if (assimilation(Food, allPipes[1])===true) {
-  //     console.log("ok")
-  //       hasAssimilated = true;
-  //       Food.clearRect();
-  //       return hasAssimilated;
-  //     }
-  // })
+
+function randomFoods () {
+  var allFoodsCopy = allFoods.slice();
+
+  for (var i = 1; i <= 1; i +=1) {
+    var index = Math.floor(Math.random() * allFoodsCopy.length);
+    movingFoods.push(new Food(N, allFoodsCopy[index].color, allFoodsCopy[index].song));
+    allFoodsCopy.splice(index, 1);
+  }
+}
 
 function updateStuff () {
     // clear old drawings from the entire canvas before drawing again
-    allPipes.forEach(function (Pipe) {
-      Pipe.drawMe();
+    allPipes.forEach(function (pipe) {
+      pipe.drawMe();
     });
 
-    allParticules.forEach(function (Particule) {
-      Particule.draw();
+    var collided = [];
+    allParticules.forEach(function (particule) {
+      particule.draw();
+      if (pipeCollision(particule)) {
+        collided.push(particule);
+        if (particule.song && particule.song !== "none") particule.song.play();
+      }
     });
 
-    if (food === 0) {
-        var index = Math.floor(Math.random() * allFoods.length) 
-        allFoods[index].drawMe()
+    var assimilated = [];
+    var oldFood = [];
+    movingFoods.forEach(function (food, index) {
+      food.drawMe();
+      if (foodAssimilation(food)) {
+        assimilated.push(food);
+        if (food.song) food.song.play();
+      }
+      if (foodAssimilation(food) || food.y > canvas.height) {
+        oldFood.push(index);
+      }
+    });
+
+    oldFood.sort(function (a, b) {
+      return b - a;
+    });
+    oldFood.forEach(function (index) {
+      movingFoods.splice(index, 1);
+    });
+
+    assimilated.forEach(function (oneFood) {
+      collided.forEach(function (oneParticle) {
+        if (oneParticle.name === oneFood.color){
+          oneParticle.color = oneFood.color
+          oneParticle.song = oneFood.song
         }
-        allFoods.forEach(function (Food) {
-          Food.drawMe();
-        });
-
-
- // ----- activate Particules ----- //
-
-    //  allParticules.forEach(function(p) {
-    //   (pipeCollision(p) === true
-    // && if 
-    // noteAssimilation(p) 
-    // && if p.name === food.color) 
-    //         console.log (p)
-    //    return  p.color = f.color
-    //     }
-    // })
+      });
+    });
 
     requestAnimationFrame(function () {
       updateStuff();
     });
+
   };
- // start the drawing loop
- 
+ // start the drawing loop 
  updateStuff();
 
-// ----- RESET ----- //
-
-
-// ----- trash ----- //
 
 
 
-      // if  (((canvas.width/2) - (innerWidth/7)-5) < this.x && this.x < ((canvas.width/2) + (innerWidth/7)+5)) {
-      //   this.creation();
-      //   return;
-      // }; 
 
-          
-    // Math.floor(Math.random() * (canvas.height-70))
-    //   if ( ((canvas.height/2) - (innerHeight/7) -5) < this.y && this.y < ((canvas.height/2) + (innerHeight/7)+5)) {
-    //     this.creation();
-    //     return;
-
-
-    //  // ----- création NOTE ----- //
-
-//  var allNotes = new Image();
-//  allNotes.src = "./imagesetsons/autocollant-mural-notes-musique-couleur-820.png";
-
-//  var note = {
-//    x: (innerWidth/2-(innerHeight/10)/2),
-//    y: 50,
-//    width: innerHeight/10,
-//    height: innerHeight/10,
-
-//    drawMe: function () {
-//      c.drawImage(allNotes, this.x, this.y, this.width, this.height);
-//    }
-//  };
-
-//    note.drawMe();
